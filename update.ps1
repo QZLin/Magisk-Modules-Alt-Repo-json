d3ef295c5a2d74acb9784574d57a3c661b0bd427
QZLin <qzlin01@163.com>
2024-07-13 15:48:09 +0800

    update

--- update.ps1
+++ update.ps1
@@ -1,3 +1,6 @@
 Invoke-WebRequest "https://ghproxy.net/raw.githubusercontent.com/Magisk-Modules-Alt-Repo/json/main/modules.json" -OutFile modules-raw.json
 $content = Get-Content modules-raw.json
-$content -replace "https://raw.githubusercontent.com", "https://ghproxy.net/raw.githubusercontent.com" | Set-Content modules.json
+$content `
+    -replace "https://raw.githubusercontent.com/", "https://ghproxy.net/raw.githubusercontent.com/" `
+    -replace "https://github.com/", "https://ghproxy.net/github.com/" `
+| Set-Content modules.json
